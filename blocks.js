daa158785122a9acbd6dd035945d8392b4922099
swedishbacon <huledule@hotmail.com>
2017-04-30 16:34:49 +0200

    more

--- blocks.js
+++ blocks.js
@@ -1,6 +1,6 @@
 ﻿function randomBlock() {
     blocktype = Math.floor(Math.random() * 5) + 1;
-    //blocktype = 5;
+    //blocktype = 7;
     rotation = 1;
     if (blocktype == 1) {
         L_block();
@@ -19,6 +19,14 @@
     }
     else if (blocktype == 5) {
         T_block();
+        color = cube5;
+    }
+    else if (blocktype == 6) {
+        Z_block();
+        color = cube5;
+    }
+    else if (blocktype == 7) {
+        inverted_Z_block();
         color = cube5;
     }
 }
@@ -73,7 +81,7 @@ function O_block() {
     playerRotationModifers[3].y = 1;
 }
 
-//O BLOCK
+//T BLOCK
 function T_block() {
     playerRotationModifers[0].x = 0;
     playerRotationModifers[0].y = 0;
@@ -83,6 +91,29 @@ function T_block() {
     playerRotationModifers[2].y = 0;
     playerRotationModifers[3].x = -1;
     playerRotationModifers[3].y = 0;
+}
+//Z BLOCK
+function Z_block() {
+    playerRotationModifers[0].x = 0;
+    playerRotationModifers[0].y = 0;
+    playerRotationModifers[1].x = -1;
+    playerRotationModifers[1].y = 0;
+    playerRotationModifers[2].x = 1;
+    playerRotationModifers[2].y = 1;
+    playerRotationModifers[3].x = 0;
+    playerRotationModifers[3].y = 1;
+}
+
+//Inverted Z BLOCK
+function inverted_Z_block() {
+    playerRotationModifers[0].x = 0;
+    playerRotationModifers[0].y = 0;
+    playerRotationModifers[1].x = 0;
+    playerRotationModifers[1].y = 1;
+    playerRotationModifers[2].x = 1;
+    playerRotationModifers[2].y = 0;
+    playerRotationModifers[3].x = -1;
+    playerRotationModifers[3].y = 1;
 }
 
 function spinBlock() {
@@ -269,7 +300,47 @@ function spinBlock() {
             playerRotationModifers[3].y = -1;
             rotation = 0;
         }
-
-
+    } else if (blocktype == 6) {
+        if (rotation == 1 || rotation == 3){
+            playerRotationModifers[0].x = 0;
+            playerRotationModifers[0].y = 0;
+            playerRotationModifers[1].x = -1;
+            playerRotationModifers[1].y = 0;
+            playerRotationModifers[2].x = 1;
+            playerRotationModifers[2].y = 1;
+            playerRotationModifers[3].x = 0;
+            playerRotationModifers[3].y = 1;
+        } else if (rotation == 2 || rotation == 4){
+            playerRotationModifers[0].x = 0;
+            playerRotationModifers[0].y = 0;
+            playerRotationModifers[1].x = 1;
+            playerRotationModifers[1].y = -1;
+            playerRotationModifers[2].x = 1;
+            playerRotationModifers[2].y = 0;
+            playerRotationModifers[3].x = 0;
+            playerRotationModifers[3].y = 1;
+            rotation = 0;
+        }
+    } else if (blocktype == 7) {
+        if (rotation == 1 || rotation == 3){
+            playerRotationModifers[0].x = 0;
+            playerRotationModifers[0].y = 0;
+            playerRotationModifers[1].x = 0;
+            playerRotationModifers[1].y = 1;
+            playerRotationModifers[2].x = 1;
+            playerRotationModifers[2].y = 0;
+            playerRotationModifers[3].x = -1;
+            playerRotationModifers[3].y = 1;
+        } else if (rotation == 2 || rotation == 4){
+            playerRotationModifers[0].x = 0;
+            playerRotationModifers[0].y = 0;
+            playerRotationModifers[1].x = 0;
+            playerRotationModifers[1].y = -1;
+            playerRotationModifers[2].x = 1;
+            playerRotationModifers[2].y = 0;
+            playerRotationModifers[3].x = 1;
+            playerRotationModifers[3].y = 1;
+            rotation = 0;
+        }
     }
 }
